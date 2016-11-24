c7a1a1135c725f97c9fb9eb3f7a588f8e63f582a
torange <18004535090@163.com>
2016-11-25 05:41:11 +0800

    first try rb tree

--- red_black_tree.c
+++ red_black_tree.c
@@ -13,19 +13,76 @@
 #include<string.h>
 #include<unistd.h>
 #include"red_black_tree.h"
-
-
-
-
-
-
-
-
-
-
-
-
-
+//init node
+Node* rb_node_init(int key,int color){
+	Node* node = (Node*)malloc(sizeof(Node));
+	if(node != NULL){
+		node->key = key;	
+		node->color = color;
+		return node;
+	}
+	return NULL;
+}
+//init tree
+Tree* rb_tree_init(){
+	Tree* tree = (Tree*)malloc(sizeof(Tree));	
+	if(tree != NULL){
+		tree->root = NULL;
+		//nil node
+		Node* node = rb_node_init(0,BLACK);
+		if(node != NULL){
+			//the nil node does not care left,key,right 
+			node->left = NULL;					
+			node->right = NULL;
+			//when insert node the root node pre is the nil node			
+			tree->nil = node;
+		}else{
+			exit(1);	
+		}
+	}
+	exit(1);
+}
+//left_rotate
+void left_rotate(Tree* tree, Node* node){
+	Node* y = node->right;	
+	node->right = y->left;
+	//y left whether null
+	if(y->left != tree->nil){//have the left side
+		y->left->pre = node;	
+	}
+	y->pre = node->pre;
+	//which side node in nodefather
+	if(node->pre == tree->nil){
+		tree->root = y;
+	}else if(node->pre->left == node){
+		node->pre->left = y;			
+	}else{
+		node->pre->right = y;	
+	} 
+	//y and node
+	y->left = node;
+	node->pre = y;
+	return ;
+}
+//right_rotate is symmetry to the left_rotate
+void right_rotate(Tree* tree, Node* node){
+	Node* y = node->left;
+	node->left = y->right;
+	if(y->right != tree->nil){
+		y->right->pre = node;	
+	}
+	y->pre = node->pre;
+	if(node->pre == tree->nil){
+		tree->root = y;	
+	}else if(node->pre->left == node){
+		node->pre->left = y;	
+	}else{
+		node->pre->right = y;	
+	}
+	y->right = node;
+	node->pre = y;
+	return ;
+}
 
 
 //test main funciton
--- red_black_tree.h
+++ red_black_tree.h
@@ -1,5 +1,30 @@
 #ifndef __RED_BLACK__
 #define __RED_BLACK__
+enum{RED=0,BLACK=1};
+typedef struct node{
+	int key;	
+	int color;
+	struct node* pre;
+	struct node* left;
+	struct node* right;
+}Node;
+typedef struct tree{
+	struct node* root;	
+	//界外节点 every leaves node left ,right side and the root node point to the nil
+	struct node* nil;
+}Tree;
+//init rb_tree
+Tree* rb_tree_init();
+//init node 
+Node* rb_node_init(int key,int color);
+//rotate旋转
+//left
+void left_rotate(Tree* tree, Node* node);
+void right_rotate(Tree* tree, Node* node);
+//insert fixup
+int rb_tree_insert_fixup(Tree* tree, Node* node);
+//insert
+int rb_tree_insert(Tree* tree, Node* node); 
 
 
 
